d7f9fee12281b061d96b9d8ab72515207b0a1b35
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-09-11 20:59:58 -0300

    Criação das colunas da Table

--- database/migrations/2026_09_11_235255_create_partidas_table.php
+++ database/migrations/2026_09_11_235255_create_partidas_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\PartidaStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,13 +14,23 @@ return new class extends Migration
     {
         Schema::create('partidas', function (Blueprint $table) {
             $table->id();
+            $table->unsignedSmallInteger('rodada');
+            
+            // Chaves Estrangeiras
+            $table->unsignedBigInteger('time_casa_id');
+            $table->unsignedBigInteger('time_fora_id');
+
+            $table->string('status', 20)->default(PartidaStatus::Agendada->value);
+            $table->datetime('data_jogo')->nullable();
+            $table->unsignedSmallInteger('gols_casa')->nullable();
+            $table->unsignedSmallInteger('gols_fora')->nullable();
             $table->timestamps();
+
+            $table->index('rodada');
+            $table->index('status');     
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('partidas');
