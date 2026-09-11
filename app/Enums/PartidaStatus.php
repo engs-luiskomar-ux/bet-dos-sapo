d6f51f4fca8648286024d5e34fce48861ab70484
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-09-11 20:51:47 -0300

    Finalização do PartidaStatus

--- app/Enums/PartidaStatus.php
+++ app/Enums/PartidaStatus.php
@@ -6,4 +6,22 @@ enum PartidaStatus: string
 {
     case Agendada = 'agendada';
     case Finalizada = 'finalizada';
+
+    /* Rotulo */
+        public function rotulo(): string
+        {
+            return match ($this){
+                self::Agendada => 'Agendada',
+                self::Finalizada => 'Finalizada',
+            };
+        }
+    
+    /* Rotulo no plural */
+    public function rotuloPlural(): string
+        {
+            return match ($this) {
+                self::Agendada => 'Agendadas',
+                self::Finalizada => 'Finalizadas',
+            };
+        }
 }
