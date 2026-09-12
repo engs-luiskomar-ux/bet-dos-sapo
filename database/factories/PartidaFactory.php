87b8ff8616ad7a0e337bc85c6475e84fc931d655
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-09-11 21:23:22 -0300

    Finalizando o PartidaFactory.php

--- database/factories/PartidaFactory.php
+++ database/factories/PartidaFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Factories;
-
+use App\Enums\PartidaStatus;
 use App\Models\Partida;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -10,15 +10,37 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class PartidaFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+        protected $model = Partida::class;
+    /** @return array<string, mixed> **/
+     
     public function definition(): array
     {
         return [
-            //
+            'rodada' => fake()->numberBetween(1, 38),
+            'time_casa_id' => fake()->numberBetween(1, 20),
+            'time_fora_id' => fake()->numberBetween(21, 40),
+            'status' => PartidaStatus::Agendada,
+            'data_jogo' => fake()->dateTimeBetween('+1 day', '+2 months'),
+            'gols_casa' => null,
+            'gols_fora' => null,
         ];
     }
+
+    /* Partida Encerrada com o placar Sorteado */
+        public function finalizada(): static 
+        {
+            return $this->state(fn (array $attributes) => [
+                'status' => PartidaStatus::Finalizada,
+                'data_jogo' => fake()->dateTimeBetween('-2 months', '-1 day'),
+                'gols_casa' => fake()->numberBetween(0, 4),
+                'gols_fora' => fake()->numberBetween(0, 4),
+            ]);
+        }
+
+        public function naRodada(int $numero): static
+        {
+            return $this->state(fn (array $attributes) => [
+                    'rodada' => $numero,
+            ]);
+        }
 }
