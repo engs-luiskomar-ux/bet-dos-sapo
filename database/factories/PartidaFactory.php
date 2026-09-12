4fe562f750a73a8a580d5afe4cca0a030b201520
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-09-11 21:12:46 -0300

    Criação da PartidaFactory

---
+++ database/factories/PartidaFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Partida;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Partida>
+ */
+class PartidaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
