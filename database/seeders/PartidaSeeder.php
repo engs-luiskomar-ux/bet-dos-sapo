2323cdfa5bb2b46aa7fcdb45a35306803c16dfb1
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-09-11 21:31:31 -0300

    Finalizando o PartidaSeeder

--- database/seeders/PartidaSeeder.php
+++ database/seeders/PartidaSeeder.php
@@ -2,16 +2,32 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Partida;
 use Illuminate\Database\Seeder;
 
+/**
+ * Dados de exemplo para conferir os filtros na tela.
+ *
+ * Rode sozinho, sem mexer no DatabaseSeeder (que e compartilhado):
+ *     php artisan db:seed --class=PartidaSeeder
+ */
 class PartidaSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        foreach (range(1, 4) as $rodada) {
+            Partida::factory()
+                ->count(5)
+                ->naRodada($rodada)
+                ->finalizada()
+                ->create();
+        }
+
+        foreach (range(5, 8) as $rodada) {
+            Partida::factory()
+                ->count(5)
+                ->naRodada($rodada)
+                ->create();
+        }
     }
 }
