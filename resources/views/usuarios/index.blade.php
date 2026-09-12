387f75ed36eda0b012769231c5a2df7463768fb5
thiago <engs-thiagoireno@camporeal.edu.br>
2026-09-12 10:33:25 -0300

    refactor: separa formulario de filtro em componente proprio

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -11,23 +11,7 @@
 
                 @include('usuarios._permissoes')
 
-                <form method="GET" action="{{ route('usuarios.index') }}" class="mb-6 flex gap-2">
-                    <input
-                        type="text"
-                        name="busca"
-                        value="{{ request('busca') }}"
-                        placeholder="Buscar por nome ou e-mail"
-                        class="border-gray-300 rounded-md shadow-sm flex-1"
-                    >
-                    <button type="submit" class="px-4 py-2 bg-gray-800 text-white rounded-md">
-                        Filtrar
-                    </button>
-                    @if(request('busca'))
-                        <a href="{{ route('usuarios.index') }}" class="px-4 py-2 text-gray-600">
-                            Limpar
-                        </a>
-                    @endif
-                </form>
+                @include('usuarios._filtros')
 
                 @if($usuarios->isEmpty())
                     <p class="text-gray-500">
