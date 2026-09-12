a0a6d40b3ee06bf1aaec142142baf1f682beb5b4
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-09-11 21:40:02 -0300

    Criação das rotas para partidas.index e partidas.show

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\PartidaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+/*
+Rotas Partidas antes do Breeze
+*/
+Route::get('/partidas', [PartidaController::class, 'index'])->name('partidas.index');
+Route::get('/partidas/{partida}', [PartidaController::class, 'show'])->name('partidas.show');
