409d9ffcefb2a4b0bcf94e7e9fa9dcd817dad0be
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-09-12 22:44:27 -0300

    definição das regras de timeRequest

--- app/Http/Requests/FiltroTimeRequest.php
+++ app/Http/Requests/FiltroTimeRequest.php
@@ -2,28 +2,33 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class FiltroTimeRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return auth()->check();
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'busca' => $this->busca
+                ? trim($this->busca)
+                : null,
+
+            'estado' => $this->estado
+                ? strtoupper(trim($this->estado))
+                : null,
+        ]);
+    }
+
     public function rules(): array
     {
         return [
-            //
+            'busca' => ['nullable', 'string', 'max:100'],
+            'estado' => ['nullable', 'string', 'size:2'],
         ];
     }
 }
