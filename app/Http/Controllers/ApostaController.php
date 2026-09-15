49c40597f53b82ebf91f8474f027c52c41755b8f
Luis <engs-luiskomar@camporeal.edu.br>
2026-09-15 12:30:18 -0300

    Store apostas

--- app/Http/Controllers/ApostaController.php
+++ app/Http/Controllers/ApostaController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ApostaRequest;
+use App\Services\ApostaService;
+use Illuminate\Http\RedirectResponse;
+
 
 class ApostaController extends Controller
 {
-    //
+    public function store(
+        ApostaRequest $request,
+        ApostaService $service
+    ): RedirectResponse {
+        $dados = $request->validated();
+
+        $service->registrar(
+            $request->user(),
+            (int) $dados['partida_id'],
+            $dados['palpite'],
+            (int) $dados['valor']
+        );
+
+        return back()->with(
+            'success',
+            'Palpite registrado com sucesso.'
+        );
+    }
 }
