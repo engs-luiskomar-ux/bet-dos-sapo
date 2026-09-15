4efc05a9317d53bd1acc400d02980fa79b1e5d05
Luis <engs-luiskomar@camporeal.edu.br>
2026-09-15 10:52:53 -0300

    Opcoes aposta

--- app/Models/Aposta.php
+++ app/Models/Aposta.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relation\BelongsTo;
 
 class Aposta extends Model
 {
-    //
+    public const OPCOES = [
+        'mandante' =>  [
+
+            'nome' => 'Mandante',
+            'multiplicador' => 2,
+        ],
+        'empate' => [
+            'nome' =>  'Empate',
+            'multiplicador' => 3,
+        ],
+        'visitante' => [
+            'nome' => 'Visitante',
+            'multiplicador' => 3,
+        ],
+    ];
+
+ 
 }
