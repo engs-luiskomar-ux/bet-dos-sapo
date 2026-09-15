bfb1431db377a2ef21b502ea84c765bcf82a34d5
Luis <engs-luiskomar@camporeal.edu.br>
2026-09-15 11:22:57 -0300

    classe apostarequest

--- app/Http/Requests/ApostaRequest.php
+++ app/Http/Requests/ApostaRequest.php
@@ -8,13 +8,11 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ApostaRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return $this->user()?->role === 'torcedor';
     }
+
 
     /**
      * Get the validation rules that apply to the request.
