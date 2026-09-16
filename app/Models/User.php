41c07d91b8aac0aecac968eab9cd9ad7294e2b62
Luis <engs-luiskomar@camporeal.edu.br>
2026-09-16 12:18:51 -0300

    Perfis usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,8 @@ use Illuminate\Notifications\Notifiable;
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
+    protected $attributes = ['role' => 'torcedor'];
+
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
