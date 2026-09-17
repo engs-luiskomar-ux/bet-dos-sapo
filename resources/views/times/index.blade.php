453dedd58739d8972c345382d38f4371778e12d9
Luis <engs-luiskomar@camporeal.edu.br>
2026-09-16 21:37:03 -0300

    Formulario filtros

--- resources/views/times/index.blade.php
+++ resources/views/times/index.blade.php
@@ -8,8 +8,26 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
+                @if ($errors->any())
+                    <div class="mb-4 rounded-md border border-red-200 bg-red-50 px-4 py-3 text-sm text-red-800">
+                        <ul class="list-inside list-disc">
+                            @foreach ($errors->all() as $erro)
+                                <li>{{ $erro }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                @include('times._filtros')
+
                 @if($times->isEmpty())
-                    <p class="text-gray-500">Nenhum time cadastrado.</p>
+                    <p class="text-gray-500">
+                        @if (request()->hasAny(['busca', 'estado']))
+                            Nenhum time encontrado para os filtros.
+                        @else
+                            Nenhum time cadastrado.
+                        @endif
+                    </p>
                 @else
                     <table class="w-full text-left">
                         <thead>
